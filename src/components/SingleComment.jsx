41f0626369c3776b6e1dcb11da9d7f5408d66bed
Andrea Maiolo <maiolo.m.2@gmail.com>
2025-05-15 17:00:46 +0200

    aggiunto bottone per cancellare commenti

--- src/components/SingleComment.jsx
+++ src/components/SingleComment.jsx
@@ -1,14 +1,35 @@
 import { Component } from "react";
-import { ListGroup } from "react-bootstrap";
+import { Button, ListGroup } from "react-bootstrap";
+import { Trash } from "react-bootstrap-icons";
 
 class SingleComment extends Component {
-  state = {
-    commentObj: this.props.commentObj,
+  handleClick = () => {
+    fetch(`https://striveschool-api.herokuapp.com/api/comments/${this.props.commentObj._id}`, {
+      method: "DELETE",
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2ODE0NzljZTFjMjUwNDAwMTUxYWI2NGUiLCJpYXQiOjE3NDczMTEyODcsImV4cCI6MTc0ODUyMDg4N30._DZV1gVqDRIqvSd0DKY6yrH7gf8IOmEaFxuIk_XOT-M",
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("errore nella cancellazione", response);
+        } else {
+          console.log("cancellato con successo", response);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
+
   render() {
     return (
       <ListGroup.Item>
-        {this.state.commentObj.comment} - rate {this.state.commentObj.rate}/5
+        {this.props.commentObj.comment} - rate {this.props.commentObj.rate}/5
+        <Button className="p-1 ms-3" onClick={this.handleClick}>
+          <Trash />
+        </Button>
       </ListGroup.Item>
     );
   }
